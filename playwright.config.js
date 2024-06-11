0257a7966b2dd2f7c5ef334b3fa43c1c16866c22
Suparna Soman <suparnasoman@Suparnas-MacBook-Pro.local>
2024-06-11 16:12:31 -0400

    headless

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-      headless: false,
+      headless: true,
       viewport: { width: 1280, height: 720 },
       actionTimeout: 10000,
       ignoreHTTPSErrors: true,
